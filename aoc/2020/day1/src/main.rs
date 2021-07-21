a1017b9aa8f32fa6274f8aec688f35b286cd50f1
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-07-21 19:32:53 +0200

    finished day1

--- aoc/2020/day1/src/main.rs
+++ aoc/2020/day1/src/main.rs
@@ -17,6 +17,24 @@ fn solve1(v: &Vec<i32>) -> i32 {
     res
 }
 
+// I guess for O(N log N) if implemented best lets look it up
+// Probably some sorting and line sweeping
+// Fuck it, I wanna write some rust for now
+//
+// Also, I should really use an Optional here
+fn solve2(v: &Vec<i32>) -> i32 {
+    for a in v {
+        for b in v {
+            for c in v {
+                if a+b+c == 2020 {
+                    return a*b*c;
+                }
+            }
+        }
+    }
+    -1
+}
+
 fn main() {
     let s = fs::read_to_string(FILENAME).expect("Something went wrong reading the file");
     let arr = s.split("\n");
@@ -32,4 +50,5 @@ fn main() {
         vec.push(i);
     }
     println!("res1: {}", solve1(&vec));
+    println!("res2: {}", solve2(&vec));
 }
