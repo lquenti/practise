aea56d7d051aff5942c29407a4b91402a99e8761
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-07-24 10:19:09 +0200

    finished day 3

--- aoc/2020/day3/src/main.rs
+++ aoc/2020/day3/src/main.rs
@@ -3,17 +3,27 @@ use std::vec::Vec;
 
 const FILENAME: &str = "input";
 
-fn solve1(v: Vec<String>) -> i32 {
+fn solve1(v: &Vec<String>, dx: usize, dy: usize) -> u64 {
     let mut res = 0;
     let mut x = 0;
-    let length = v[0].chars().count();
-    for line in v {
-        if line.chars().nth(x).unwrap() == '#' {
+    let mut y = 0;
+    let xlen = v[0].chars().count();
+    let ylen = v.len();
+    loop {
+        if y >= ylen {
+            break;
+        }
+        if v[y].chars().nth(x).unwrap() == '#' {
             res+=1;
         }
-        x = (x+3)%length;
+        x = (x+dx)%xlen;
+        y+=dy;
     }
     res
+}
+
+fn solve2(v: &Vec<String>) -> u64 {
+    solve1(v,1,1) * solve1(v,3,1) * solve1(v,5,1) * solve1(v,7,1) * solve1(v,1,2)
 }
 
 fn main() {
@@ -28,5 +38,6 @@ fn main() {
         }
         v.push(String::from(s));
     }
-    println!("res1: {}", solve1(v));
+    println!("res1: {}", solve1(&v, 3,1));
+    println!("res2: {}", solve2(&v));
 }
