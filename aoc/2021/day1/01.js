d739596a8ce90ecd286861c5cc1f7be8e83d62fc
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-12-01 22:25:06 +0100

    :zap: FP BOYS

--- aoc/2021/day1/01.js
+++ aoc/2021/day1/01.js
@@ -1,16 +1,26 @@
 const fs = require("fs");
-const input = fs.readFileSync(process.cwd() + "/input1").toString();
-
-let res = 0;
-
-input
+const input = fs
+  .readFileSync(process.cwd() + "/input1")
+  .toString()
   .split("\n")
-  .filter((val) => val.trim() !== "")
-  .map((val) => parseInt(val))
-  .reduce((prev, curr) => {
+  .filter((val) => val.trim !== "")
+  .map((val) => parseInt(val));
+
+const solve = (xs) => {
+  let res = 0;
+
+  xs.reduce((prev, curr) => {
     if (curr > prev) {
       res += 1;
     }
     return curr;
   });
-console.log(res);
+  console.log(res);
+};
+
+solve(input);
+solve(
+  input
+    .map((_, i) => input[i] + input[i + 1] + input[i + 2])
+    .filter((v) => v !== undefined)
+);
