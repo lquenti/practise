a77ba6f04655f996cebecca618050ad890fe9256
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2021-12-02 16:41:48 +0100

    :zap: Not as functional but gets the job done

--- aoc/2021/day2/02.js
+++ aoc/2021/day2/02.js
@@ -5,7 +5,7 @@ const input = fs
   .split("\n")
   .slice(0, -1);
 
-const height = input
+const height1 = input
   .filter((v) => v.startsWith("up") || v.startsWith("down"))
   .map((v) => parseInt(v.split(" ")[1]) * (v.startsWith("up") ? -1 : 1))
   .reduce((a, b) => a + b);
@@ -15,4 +15,27 @@ const length = input
   .map((v) => parseInt(v.split(" ")[1]))
   .reduce((a, b) => a + b, 0);
 
-console.log(height * length);
+console.log(height1 * length);
+
+const solution2 = input.reduce(
+  (a, v) => {
+    const n = parseInt(v.split(" ")[1]);
+    if (v.startsWith("up")) {
+      return { ...a, aim: a.aim - n };
+    } else if (v.startsWith("down")) {
+      return { ...a, aim: a.aim + n };
+    }
+    return {
+      ...a,
+      horizontal: a.horizontal + n,
+      depth: a.depth + a.aim * n,
+    };
+  },
+  {
+    horizontal: 0,
+    depth: 0,
+    aim: 0,
+  }
+);
+
+console.log(solution2.horizontal * solution2.depth);
