38af050c06778533907109cc3cf7210f41c211cd
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-05-03 09:14:16 +0200

    USACO template

---
+++ templates/USACO.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define PROBLEM "paint"
+
+void solve() {
+  freopen(PROBLEM ".in", "r", stdin);
+  freopen(PROBLEM ".out", "w", stdout);
+  // DO THE SOLVING HERE
+}
+
+int main() {
+  solve();
+}
