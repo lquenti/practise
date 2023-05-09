11f58e6bbcc9b1bab2955159c94707a4afbfe64d
julian <jlagg97@gmail.com>
2023-05-09 21:21:53 +0200

    blockgame from usaco guide starred problems

---
+++ USACO/BlockGame/blockgame.cc
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define PROBLEM "blocks"
+
+#define ll long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+
+#define P(X) cerr << #X << "=" << X << "; ";
+#define NL cerr << endl << endl
+
+void solve() {
+    freopen(PROBLEM ".in", "r", stdin);
+    freopen(PROBLEM ".out", "w", stdout);
+    // DO THE SOLVING HERE
+    ll N;
+    cin >> N;
+    array<int, 26> xs = {0};
+    string f,b;
+    while (N--) {
+        array<int, 26> front = {0};
+        array<int, 26> back = {0};
+        cin >> f >> b;
+        for (char c: f) {
+           front[c-'a']++;
+        }
+        for (char c: b) {
+            back[c-'a']++;
+        }
+        for (size_t i=0; i<26; ++i) {
+            xs[i] += max(front[i], back[i]);
+        }
+    }
+
+    for (size_t i=0; i< 26; ++i) {
+        cout << xs[i] << endl;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
