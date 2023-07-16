e7364eff706d493858f8de37894217243e265ace
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-07-16 10:40:10 +0200

    finally had inet to commit

--- aoc/2022/01/01.py
+++ aoc/2022/01/01.py
@@ -1,3 +1,4 @@
+# pt1
 with open("./input", "r") as fp:
     best_known = 0
     current = 0
@@ -8,3 +9,16 @@ with open("./input", "r") as fp:
         else:
             current += int(line)
     print(f"pt1: {best_known}")
+
+# pt2
+with open("./input", "r") as fp:
+    all_elvs = []
+    current = 0
+    for line in fp.readlines():
+        if line.strip() == "":
+            all_elvs.append(current)
+            current = 0
+        else:
+            current += int(line)
+    all_elvs.sort()
+    print(f"pt2: {sum(all_elvs[-3:])}")
