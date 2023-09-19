dc43fd2db0e6a21d1c112a6630ce75f4030025d2
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-09-19 21:25:02 +0200

    evolution

--- USACO/Evolution/evolution.cc
+++ USACO/Evolution/evolution.cc
@@ -19,8 +19,8 @@ void solve() {
     int N;
     cin >> N;
 
-    unordered_set<string> props{};
-    vector<unordered_set<string>> sub_pops{};
+    set<string> props{}; /* All props that exist at all */
+    vector<set<string>> sub_pops{}; /* each population and their props */
     for (int i = 0; i < N; i++) {
         int K;
         cin >> K;
@@ -34,43 +34,66 @@ void solve() {
         }
     }
 
-    // FIND UNIQUE
-    int unique_one = -1;
-    string unique_prop = nullptr;
-    for (auto p: props) {
-        int occ = -1; // not in
-        for (int i = 0; i < N; i++) {
-            if (sub_pops[i].find(p) != sub_pops[i].end()) {
-                if (occ != -1) {
-                    // found it second time
-                    goto not_unique;
-                } else {
-                    occ = i;
-                    unique_prop = p;
+    while (true) {
+        // FIND UNIQUE
+        int unique_one = -1;
+        string_view unique_prop{};
+        for (auto p: props) {
+            int occ = -1; // not in
+            for (int i = 0; i < N; i++) {
+                if (sub_pops[i].find(p) != sub_pops[i].end()) {
+                    if (occ != -1) {
+                        // found it second time
+                        goto not_unique;
+                    } else {
+                        occ = i;
+                        unique_prop = p;
+                    }
                 }
             }
+            if (occ != -1) {
+                unique_one = occ;
+                break;
+            }
+            not_unique:;
         }
-        if (occ != -1) {
-            unique_one = occ;
-        }
-        not_unique:
-        ;
-    }
 
-    // if no unique found, then it is invalid
-    if (unique_one == -1) {
-        cout << "no" << endl;
+        for (int i = 0; i < N; ++i) {
+            if (sub_pops[i].size() > 0) {
+                goto not_all_clear;
+            }
+        }
+        cout << "yes" << endl;
         return;
+        not_all_clear:
+
+        // if no unique found, then it is invalid
+        if (unique_one == -1) {
+            cout << "no" << endl;
+            return;
+        }
+
+        // Dont get a dangling ref if bad delete
+        string unique{unique_prop};
+
+        // remove prop from unique subpop
+        sub_pops[unique_one].erase(unique);
+
+        // merge if possible (okay if not)
+        for (int i = 0; i < N; ++i) {
+            if (i == unique_one) {
+                continue;
+            }
+            if (sub_pops[i] == sub_pops[unique_one]) {
+                sub_pops[i].clear();
+            }
+        }
     }
-
-    // remove prop from unique subpop
-
-    props.erase()
-    // merge if possible (okay if not)
 }
 
-
-}
+// B C
+// B C
+// D C X
 
 
 int main() {
