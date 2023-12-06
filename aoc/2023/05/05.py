bd98cee6f4b169ab6ece01d74e538a6ec08b697b
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-12-06 14:02:55 +0100

    5.1

---
+++ aoc/2023/05/05.py
@@ -0,0 +1,46 @@
+import sys
+
+def solve01():
+    with open(sys.argv[1], "r") as fp:
+        lines = fp.read()
+    
+    xs = lines.split("\n\n")
+    seeds_str = xs[0]
+    maps = xs[1:]
+
+    # extract seeds:
+    seeds_str = seeds_str.split(":")[1]
+    seeds = [int(x) for x in seeds_str.split(" ") if x.strip() != ""]
+
+    # extract maps
+    def extract_map(m_str):
+        # remove headline
+        ms = m_str.split("\n")[1:]
+        return [[int(x) for x in m.split(" ") if x.strip() != ""] for m in ms if m.strip() != ""]
+    maps = [extract_map(m) for m in maps]
+
+    def eval_map(m, x):
+        for dst_rng, src_rng, rng_len in m:
+            # if it is in our range
+            if src_rng <= x < (src_rng + rng_len):
+                # Formula (solving for Y)
+                # src_rng+Y=x
+                # Y=x-src_rng
+                #
+                # Thus we want to return
+                # dst_rng+Y = dst_len+x-src_rng
+                return dst_rng + x - src_rng
+            # else we do not have a fit
+        # not found, thus `id`
+        return x
+
+    for m in maps:
+        seeds = [eval_map(m, x) for x in seeds]
+    print(min(seeds))
+
+
+
+
+
+
+solve01()
