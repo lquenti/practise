28815d106f1cf1651c0cdc0c7d6fa6653f22850a
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-01-26 21:21:25 +0100

    insertion sort

--- other/algos/sorting.c
+++ other/algos/sorting.c
@@ -36,12 +36,13 @@ void bubble_sort(void *base, size_t nmemb, size_t size,
                  int (*cmp)(const void *, const void *)) {
   size_t i, j;
   void *tmp;
+  char *basec;
   
   /* To avoid void* arithmetic:
    * Note that char is defined as the smallest addressable unit
    * thus it is perfect for pointer arithmetic
    */
-  char *basec = (char *)base;
+  basec = (char *)base;
 
   tmp = malloc(size);
 
@@ -52,16 +53,40 @@ void bubble_sort(void *base, size_t nmemb, size_t size,
       }
     }
   }
+
   free(tmp);
 }
 
 void insertion_sort(void *base, size_t nmemb, size_t size,
                  int (*cmp)(const void *, const void *)) {
+  size_t i, j;
+  void *tmp;
+  char *basec;
+
+  basec = (char *)base;
+  tmp = malloc(size);
+  
+  for (i=1; i<nmemb; ++i) {
+    for (j=0; j<i; ++j) {
+      if (cmp(basec+j*size, basec+i*size) > 0) {
+        swap(basec+j*size, basec+i*size, size);
+      }
+    }
+  }
+
+  free(tmp);
 }
 
 int main() {
   size_t i;
   i32 xs[] = {9,2,4,1,6,5};
+  bubble_sort(xs, 6, sizeof(i32), cmp_i32);
+  for (i=0; i<6; ++i) {
+    printf("%d ", xs[i]);
+  }
+  printf("\n");
+
+  xs[0]=9; xs[1]=2; xs[2]=4; xs[3]=1; xs[4]=6; xs[5]=5;
   bubble_sort(xs, 6, sizeof(i32), cmp_i32);
   for (i=0; i<6; ++i) {
     printf("%d ", xs[i]);
